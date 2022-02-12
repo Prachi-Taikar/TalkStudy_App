2bbd22a02892274bd03dfb57e64a156364061b40
Prachi <prachitaikar1994@gmail.com>
2022-02-12 13:00:03 +0530

    crud update

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
 
@@ -21,5 +21,28 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
+
+    if request.method =='POST':
+        # print(request.POST)
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {'form':form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance = room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance = room)
+
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+            
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
