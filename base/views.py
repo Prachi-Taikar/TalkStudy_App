731eb09116f646be8825eaf8b59b14d78f86b044
Prachi <prachitaikar1994@gmail.com>
2022-02-15 21:44:00 +0530

    Flash Messages and User Logout

--- base/views.py
+++ base/views.py
@@ -3,6 +3,8 @@ from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
 # rooms =[
@@ -20,12 +22,22 @@ def loginPage(request):
         try:
             user = User.objects.get(username=username)
         except:
-            pass
+            messages.error(request,'User Does Not Exist.')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else: 
+            messages.error(request, 'Username or Password Does Not Exist.')
 
     context = {}
     return render(request, 'base/login_register.html', context)
 
-
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
